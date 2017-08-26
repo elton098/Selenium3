6ff805d29bbca0d169042e0bef921d7138cb7870
elton098 <123ekene@gmail.com>
2017-08-26 13:54:06 -0400

    selenium setup

--- src/test/java/MAinPage_Test.java
+++ src/test/java/MAinPage_Test.java
@@ -1,14 +1,23 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
-/**
- * Created by E on 8/12/2017.
- */
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
 public class MAinPage_Test extends SelDriver {
 
+    //veirfy that the user can search for something on google
     @Test
-    public void goToGoogle(){
+    public void verifyGoogleSearch(){
         driver.get("http://www.google.com");
-        driver.get("");
-        ///DSJKHBFKJDSBHFJKDSBKJFESADBIOUFSZ
+        driver.findElement(By.id("lst-ib")).sendKeys("Rolan cut class for 8 months and i'm definitely not pissed");
+        driver.findElement(By.name("btnK")).click();
+        assert(driver.findElement(By.xpath(".//*[@id='rso']/div/div/div[1]/div/div/h3/a")).getText().contains("Rolan"));
+        assert (driver.findElement(By.xpath(".//*[@id='rso']/div/div/div[1]/div/div/h3/a")).isDisplayed());
+        assertEquals("rolan","ROLAN");
+        assertTrue(true);
     }
 }
